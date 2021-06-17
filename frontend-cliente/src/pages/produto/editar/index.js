26182c451ae5019c858884d627e7ca7b5f545561
LeonardoDJS <leosantos.dev@gmail.com>
2021-06-16 22:47:57 -0300

    teste13

---
+++ frontend-cliente/src/pages/produto/editar/index.js
@@ -0,0 +1,163 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import './index.css';
+ 
+class EditarProduto extends Component {
+    constructor(props) {
+        super(props);
+ 
+        this.state = {
+            produto: {
+                nome: "",
+                descricao: "",
+                preco: "",
+                estoque: ""
+            },
+            erro: null,
+            redirect: false
+        };
+    }
+ 
+    exibeErro() {
+        const { erro } = this.state;
+ 
+        if (erro) {
+            return (
+                <div className="alert alert-danger" role="alert">
+                    Erro de conexão com o servidor
+                </div>
+            );
+        }
+    }
+ 
+    componentDidMount() {
+        const { id } = this.props.match.params;
+ 
+        fetch(`https://db-prova-1.herokuapp.com/produto/${id}`)
+            .then(data => {
+                data.json().then(data => {
+                    if (data.error) {
+                        this.setState({ erro: data.error });
+                    } else {
+                        this.setState({ produto: data });
+                    }
+                });
+            })
+            .catch(erro => this.setState({ erro: erro }));
+    }
+ 
+    render() {
+        const { redirect } = this.state;
+ 
+        if (redirect) {
+            return <Redirect to="/produtos" />;
+        } else {
+            return (
+                <form onSubmit={this.handleSubmit}>
+                    <fieldset>
+                        <legend>Criar Usuário</legend>
+                        <div className="produto-update">
+                            <label htmlFor="nome">Nome </label>
+                            <br />
+                            <input
+                                type="string"
+                                id="nome"
+                                name="nome"
+                                placeholder="Nome"
+                                minLength="3"
+                                maxLength="100"
+                                required
+                                value={this.state.produto.nome}
+                                onChange={this.handleInputChange}
+                            />
+                        </div>
+                        <div className="produto-update">
+                        <label htmlFor="descricao">Descrição </label>
+                            <br />
+                            <input
+                                type="string"
+                                id="descricao"
+                                name="descricao"
+                                placeholder="Descrição do Produto"
+                                required
+                                value={this.state.produto.descricao}
+                                onChange={this.handleInputChange}
+                            />
+                        </div>
+                        <div className="produto-update">
+                        <label htmlFor="preco">Preço </label>
+                            <br />
+                            <input
+                                type="double"
+                                id="preco"
+                                name="preco"
+                                placeholder="Preço do Produto"
+                                required
+                                value={this.state.produto.preco}
+                                onChange={this.handleInputChange}
+                            />
+                        </div>
+
+                        <div className="produto-update">
+                            <label htmlFor="preco">Estoque </label>
+                            <br />
+                            <input
+                                type="string"
+                                id="estoque"
+                                name="estoque"
+                                placeholder="Estoque"
+                                required
+                                value={this.state.produto.estoque}
+                                onChange={this.handleInputChange}
+                            />
+                        </div>
+ 
+                        <button type="submit" className="btn btn-primary">
+                            Atualizar
+                    </button>
+                    </fieldset>
+                </form>
+            );
+        }
+    }
+ 
+ 
+ 
+    handleInputChange = event => {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+ 
+        this.setState(prevState => ({
+            produto: { ...prevState.produto, [name]: value }
+        }));
+    };
+ 
+    handleSubmit = event => {
+        const { id } = this.state.produto;
+ 
+        fetch(`https://produto-backend-p1.herokuapp.com/produto/${id}`, {
+            method: "put",
+            body: JSON.stringify(this.state.produto),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(data => {
+                if (data.ok) {
+                    this.setState({ redirect: true });
+                } else {
+                    data.json().then(data => {
+                        if (data.error) {
+                            this.setState({ erro: data.error });
+                        }
+                    });
+                }
+            })
+            .catch(erro => this.setState({ erro: erro }));
+ 
+        event.preventDefault();
+    };
+}
+ 
+export default EditarProduto;
